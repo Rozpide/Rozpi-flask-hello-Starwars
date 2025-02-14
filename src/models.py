d7b3d665aea3fc13ca084ae61ec1569276a7661f
Rozpi <100348581+Rozpide@users.noreply.github.com>
2025-02-14 23:00:47 +0000

    segundo commit

--- src/models.py
+++ src/models.py
@@ -1,9 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_swagger import swagger
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -14,6 +18,7 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
